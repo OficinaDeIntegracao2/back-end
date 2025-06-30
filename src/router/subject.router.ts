61efb9ca434944a2724263691c74b1d47f560beb
Douglas Ribeiro <dgrb0325@gmail.com>
2025-06-30 20:59:33 -0300

    feat: add admin can create a subject

--- src/router/subject.router.ts
+++ src/router/subject.router.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import Router from "./router";
-import { SubjectController } from "@controller/subject.controlller";
 import { injectable } from "tsyringe";
 import AuthorizeMiddleware from "../middleware/authorize.middleware";
 import EnsureSameUserOrAdminMiddleware from "../middleware/ensure-same-user-or-admin.middleware";
+import { SubjectController } from "@controller/subject.controller";
 
 @injectable()
 export default class SubjectRouter implements Router {
@@ -15,7 +15,7 @@ export default class SubjectRouter implements Router {
 
   get = (): express.Router => {
     const router = express.Router({ mergeParams: true });
-    router.post("/", this.authorizationMiddleware.authorize(["PROFESSOR"]), this.subjectController.create);
+    router.post("/", this.authorizationMiddleware.authorize(["ADMIN", "PROFESSOR"]), this.subjectController.create);
     router.get("/", this.authorizationMiddleware.authorize(["ADMIN", "PROFESSOR"]), this.ensureSameUserOrAdminMiddleware.validate(), this.subjectController.getAllProfessorSubjects);
     router.get("/:subjectId", this.authorizationMiddleware.authorize(["ADMIN", "PROFESSOR", "VOLUNTEER"]), this.ensureSameUserOrAdminMiddleware.validate(), this.subjectController.getById);
     router.patch("/:subjectId", this.authorizationMiddleware.authorize(["ADMIN", "PROFESSOR"]), this.ensureSameUserOrAdminMiddleware.validate(), this.subjectController.updateById);
